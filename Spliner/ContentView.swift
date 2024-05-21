259e7565b9bf4e78fe9544ab9f1299c0c5ccd95e
hypebeans <24372255+hypebeans@users.noreply.github.com>
2024-05-21 13:19:49 +0900

    Initial Commit

---
+++ Spliner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spliner
+//
+//  Created by Jonathan Budiman on 2024/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spliner/SplinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplinerApp.swift
+//  Spliner
+//
+//  Created by Jonathan Budiman on 2024/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SplinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
