60bc7ee6132c8a9689ecb29fd38d245249b0639b
hypebeans <24372255+hypebeans@users.noreply.github.com>
2024-05-21 14:08:45 +0900

    Initial commit
    Added Spline assets, including the screenshots 🐱

--- Spliner/ContentView.swift
+++ Spliner/ContentView.swift
@@ -5,20 +5,44 @@
 //  Created by Jonathan Budiman on 2024/05/21.
 //
 
+import SplineRuntime
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            ChibiTori()
+                
+            VStack {
+                Text("うさぎとちび鳥")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Image("KawaiiUsagi")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .scaleEffect(0.75)
+            }
+            
+            .padding()
+        }.ignoresSafeArea(.all)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ChibiTori: View {
+    var body: some View {
+        
+        let url = URL(string: "https://build.spline.design/hW4zIadefMxR4lsgWudI/scene.splineswift")!
+
+        // // fetching from local
+        // let url = Bundle.main.url(forResource: "scene", withExtension: "splineswift")!
+
+        try? SplineView(sceneFileURL: url)
+    }
 }
